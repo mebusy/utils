217c41cb717e739f22371a847208a4ed8a4089e8
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2025-01-14 16:03:55 +0800

    +chattts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # utils
-powerful utils
+
+
+tool | description  | misc
+--- | --- | ---
+chattts.sh | Text-to-speech | https://github.com/2noise/ChatTTS
+
+
---
+++ chattts.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# repo: https://github.com/2noise/ChatTTS
+
+set -e
+
+envFolder=chattts
+
+alias pythonExec=python3.10
+
+# if not exist, create a new virtual environment
+if [ ! -d "$envFolder" ]; then
+    echo "Creating virtual environment..."
+    python3.10 -m venv $envFolder
+fi
+
+
+# if $VIRTUAL_ENV  has suffix  $envFolder
+if [[ "$VIRTUAL_ENV" == *"$envFolder" ]]; then
+    # if chattts is not installed, install it
+    if ! pythonExec -m pip show chattts-fork > /dev/null; then
+        echo "Installing chattts..."
+        pythonExec -m pip install numpy==1.24
+        pythonExec -m pip install -U chattts-fork
+
+        echo "now try it:"
+        echo '$' "chattts '你好,你好' --seed=1234 -o 123.mp3"
+    fi
+else
+    # env is not activated, activate it
+    echo "run following command to activate the environment:"
+    echo '$' source $envFolder/bin/activate
+    echo "then run this script again."
+fi
+
+
+
+
+
+
